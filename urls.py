e1d265f72c016ff506e9f24a736c8ad481bc21a5
namratathese <152158207+namratathese@users.noreply.github.com>
2023-12-04 12:54:28 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,16 @@
 #from django.contrib import admin
 from django.urls import path,include
-from watchlist_app.api.views import movie_list,movie_details
+#from watchlist_app.api.views import movie_list,movie_details   #=======fun based views name========
+from watchlist_app.api.views import WatchListAV,WatchDetailAV,StreamPlatformAV,StreamPlatformDetailAV #=======class views name========
+
+#========[MovieListAV,MovieDetailAV]=============
+
 
 urlpatterns = [
-    path('list/', movie_list,name='movie-list'),
-    path('<int:pk>',movie_details,name='movie-detail'),
-]
+    path('list/', WatchListAV.as_view(),name='movie-list'),
+    path('<int:pk>',WatchDetailAV.as_view(),name='movie-detail'),
+    path('stream/',StreamPlatformAV.as_view(),name='stream'),
+    path('stream/<int:pk>',StreamPlatformDetailAV.as_view(),name='stream-detail'),
+
+]   
+
